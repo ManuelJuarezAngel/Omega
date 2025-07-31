56628daffa6cf2e526fb9f5343fefba4d06247b6
Manuel JA <manuelangel010625@gmail.com>
2025-07-31 14:19:34 -0600

    Se ajusto el section

--- src/components/Services/ServicesSection.jsx
+++ src/components/Services/ServicesSection.jsx
@@ -21,25 +21,28 @@ const services = [
 
 function ServicesSection() {
   return (
-    <section className="container mx-auto px-4 py-12">
-      <div className="text-left mb-8">
-        <h2 className="text-[16px] font-semibold text-[#FF6700] mb-0">
-          NUESTROS SERVICIOS
-        </h2>
-        <h1 className="text-[40px] max-w-[450px] relative font-bold text-[#0C152B] mt-0 leading-tight">
-          Ofrecemos una amplia variedad de Servicios
-        </h1>
-      </div>
+    <section className="w-full flex justify-center bg-base-200 p-3">
+      <div className="max-w-7xl w-[100%] flex justify-start items-center gap-4 flex-col my-10">
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {services.map((svc) => (
-          <ServicesCard
-            key={svc.title}
-            image={svc.image}
-            title={svc.title}
-            description={svc.description}
-          />
-        ))}
+        <div className="w-[100%] flex justify-start items-start flex-col">
+          <h2 className="text-xs font-bold text-[#FF6700] mb-0">NUESTROS SERVICIOS</h2>
+          <div className="max-w-80">
+            <h2 className="text-3xl font-bold text-[#0C152B] mt-0 leading-tight">Ofrecemos una amplia variedad de Servicios</h2>
+          </div>
+          
+        </div>
+
+        <div className="w-[100%] grid grid-cols-1 md:grid-cols-3 gap-6">
+          {services.map((svc) => (
+            <ServicesCard
+              key={svc.title}
+              image={svc.image}
+              title={svc.title}
+              description={svc.description}
+            />
+          ))}
+        </div>
+
       </div>
     </section>
   );
