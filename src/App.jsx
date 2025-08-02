5c8346f7d3d80d7f80aa3d140ef24d0babaf62df
DannAv <dancrkyt@gmail.com>
2025-08-01 19:27:52 -0600

    seccion de clientes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import AOS from "aos"
 import 'aos/dist/aos.css'
 import ServicesSection from "./components/Services/ServicesSection.jsx";
+import ClientsSection from "./components/clientes/ClientsSection .jsx";
 
 
 
@@ -41,6 +42,7 @@ function App() {
     <div className={`relative min-h-screen ${theme === "business" ? "text-white bg-gray-900" : "bg-base-200"}`}>
       <Navbar />
       <ServicesSection />
+      <ClientsSection />
       <Footer />
     </div>
   );
